d6349fd7eb4191664489558eee67bda38a3a8dc1
rgeron <raphaelgeron123@gmail.com>
2025-05-20 14:27:45 +0200

    cool

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
-          {children}
+          <div className="pt-24">{children}</div>
 
           <FooterSection />
         </ThemeProvider>
