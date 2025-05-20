37ec771dea6290cfeaecf062820c0a4a126e37aa
rgeron <raphaelgeron123@gmail.com>
2025-05-20 15:43:34 +0200

    script for tool

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { FooterSection } from "../src/components/sections/footer-section";
 import "./globals.css";
 
@@ -32,6 +33,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://sdk.hellouniweb.com/base/main.js"
+          data-account="lZOzpEvf"
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
